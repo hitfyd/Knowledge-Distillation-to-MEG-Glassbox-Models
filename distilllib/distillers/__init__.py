f13bc92a3f4819234c9dcc04149647c8cee04500
hitfyd <hitfyd@foxmail.com>
2023-05-06 21:51:32 +0800

    CamCAN数据集测试KD和Vanilla

--- distilllib/distillers/__init__.py
+++ distilllib/distillers/__init__.py
@@ -1,11 +1,9 @@
 from ._base import Vanilla
 from .KD import KD
-from .KDSVD import KDSVD
 from .DKD import DKD
 
 distiller_dict = {
     "NONE": Vanilla,
     "KD": KD,
-    "KDSVD": KDSVD,
     "DKD": DKD,
 }
