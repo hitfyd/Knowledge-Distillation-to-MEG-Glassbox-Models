af2d436b5795b120e37d1944ef8143a07742ff15
hitfyd <hitfyd@foxmail.com>
2023-06-27 15:01:57 +0800

    修复了通道特征归因多模型实际上只记录了第一个模型结果的bug

--- Run-Script/attribution.py
+++ Run-Script/attribution.py
@@ -16,10 +16,10 @@ setup_seed(RAND_SEED)
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 # 要解释的样本数据集
-# dataset = 'CamCAN'  # CamCAN DecMeg2014
-# channels, points, num_classes = 204, 100, 2
-dataset = 'DecMeg2014'  # CamCAN DecMeg2014
-channels, points, num_classes = 204, 250, 2
+dataset = 'CamCAN'  # CamCAN DecMeg2014
+channels, points, num_classes = 204, 100, 2
+# dataset = 'DecMeg2014'  # CamCAN DecMeg2014
+# channels, points, num_classes = 204, 250, 2
 dataset_path = '../dataset/{}_test.npz'.format(dataset)
 data, labels = get_data_labels_from_dataset(dataset_path)
 sample_num, channels, points = data.shape
@@ -29,22 +29,21 @@ if dataset == 'DecMeg2014':
     label_names = ['Scramble', 'Face']
 
 # 恢复预训练模型
-teacher_model = hgrn(channels=channels, points=points,
-                     num_classes=num_classes)  # DecMeg2014数据集上以FAKD效果最好的HGRN为例，CamCAN以VARCNN为例
-# teacher_model = varcnn(channels=channels, points=points, num_classes=num_classes)  # DecMeg2014数据集上以FAKD效果最好的HGRN为例，CamCAN以VARCNN为例
+# teacher_model = hgrn(channels=channels, points=points, num_classes=num_classes)  # DecMeg2014数据集上以FAKD效果最好的HGRN为例，CamCAN以VARCNN为例
+teacher_model = varcnn(channels=channels, points=points, num_classes=num_classes)  # DecMeg2014数据集上以FAKD效果最好的HGRN为例，CamCAN以VARCNN为例
 student_model_vanilla = sdt(channels=channels, points=points, num_classes=num_classes)  # 没有经过知识蒸馏的SDT
 student_model_fakd = sdt(channels=channels, points=points, num_classes=num_classes)  # 纯FAKD，无CE损失
 student_model_fakd_ce = sdt(channels=channels, points=points, num_classes=num_classes)  # 具有CE
 
-pretrain_teacher_model_path = "../checkpoint/Models_Train/DecMeg2014_HGRN_20220616192753_checkpoint.pt"
-pretrain_student_model_vanilla_path = "../checkpoint/Models_Train/DecMeg2014_SDT_student"
-pretrain_student_model_fakd_path = "../checkpoint/Models_Train/DecMeg2014_SDT_ShapleyFAKD-CE"
-pretrain_student_model_fakd_ce_path = "../checkpoint/Models_Train/DecMeg2014_SDT_ShapleyFAKD"
+# pretrain_teacher_model_path = "../checkpoint/Models_Train/DecMeg2014_HGRN_20220616192753_checkpoint.pt"
+# pretrain_student_model_vanilla_path = "../checkpoint/Models_Train/DecMeg2014_SDT_student"
+# pretrain_student_model_fakd_path = "../checkpoint/Models_Train/DecMeg2014_SDT_ShapleyFAKD-CE"
+# pretrain_student_model_fakd_ce_path = "../checkpoint/Models_Train/DecMeg2014_SDT_ShapleyFAKD"
 
-# pretrain_teacher_model_path = "../checkpoint/Models_Train/CamCAN_VARCNN_20220616160458_checkpoint.pt"
-# pretrain_student_model_vanilla_path = "../checkpoint/Models_Train/CamCAN_SDT_student"
-# pretrain_student_model_fakd_path = "../checkpoint/Models_Train/CamCAN_SDT_ShapleyFAKD"
-# pretrain_student_model_fakd_ce_path = "../checkpoint/Models_Train/CamCAN_SDT_ShapleyFAKD_CE"
+pretrain_teacher_model_path = "../checkpoint/Models_Train/CamCAN_VARCNN_20220616160458_checkpoint.pt"
+pretrain_student_model_vanilla_path = "../checkpoint/Models_Train/CamCAN_SDT_student"
+pretrain_student_model_fakd_path = "../checkpoint/Models_Train/CamCAN_SDT_ShapleyFAKD-CE"
+pretrain_student_model_fakd_ce_path = "../checkpoint/Models_Train/CamCAN_SDT_ShapleyFAKD"
 
 teacher_model.load_state_dict(load_checkpoint(pretrain_teacher_model_path))
 student_model_vanilla.load_state_dict(load_checkpoint(pretrain_student_model_vanilla_path))
@@ -57,24 +56,23 @@ model_name_list = [pretrain_teacher_model_path.split('/')[-1], pretrain_student_
 
 db_path = '../record/{}_benchmark'.format(dataset)
 db = shelve.open(db_path)
-window_length = 10
-M = 8
-reference_num = 100
-reference_dataset = data[-reference_num:]
-for sample_id in range(sample_num - reference_num):
-    origin_input, truth_label = data[sample_id], labels[sample_id]
-    attribution_map_lists = shapley_fakd_parallel(origin_input, model_list, reference_dataset,
-                                                  classes=classes, window_length=window_length, M=M)
-    for model_id in range(len(model_list)):
-        model = model_list[model_id]
-        origin_pred = predict(model, origin_input.reshape(1, channels, points))
-        origin_pred_label = origin_pred.max(1)[1]
-        origin_pred = origin_pred.detach().cpu().numpy()
-        origin_pred_label = origin_pred_label.detach().cpu().numpy()
-        result = AttributionResult(dataset, label_names, sample_id, origin_input, truth_label,
-                                   model_name_list[model_id], origin_pred, origin_pred_label,
-                                   attribution_map_lists[model_id])
-        db[result.result_id] = result
+batch_size = 64
+M = 16
+# for sample_id in range(0, sample_num, batch_size):
+#     origin_input, truth_label = data[sample_id:sample_id + batch_size], labels[sample_id:sample_id + batch_size]
+#     features_lists = shapley_fakd_parallel(origin_input, model_list, M=M)
+#     for model_id in range(len(model_list)):
+#         model = model_list[model_id]
+#         origin_pred = predict(model, origin_input)
+#         origin_pred_label = origin_pred.max(1)[1]
+#         origin_pred = origin_pred.detach().cpu().numpy()
+#         origin_pred_label = origin_pred_label.detach().cpu().numpy()
+#         for batch_id in range(0, len(origin_input)):
+#             result = AttributionResult(dataset, label_names, sample_id + batch_id,
+#                                        origin_input[batch_id], truth_label[batch_id],
+#                                        model_name_list[model_id], origin_pred[batch_id], origin_pred_label[batch_id],
+#                                        features_lists[model_id].detach().cpu().numpy()[batch_id])
+#             db[result.result_id] = result
 
 # 读取通道可视化信息
 channel_db = shelve.open('../dataset/grad_info')
@@ -82,24 +80,23 @@ channels_info = channel_db['info']
 channel_db.close()
 
 heatmap_channel_list = []
-for mean_class in [0, 1]:
+for mean_class in [0]:
     for model_id in range(len(model_list)):
         result_list = []
         for sample_id in range(sample_num):
             result_id = "{}_{}_{}".format(dataset, sample_id, model_name_list[model_id])
             result = db[result_id]
-            if result.pred_label == result.truth_label == mean_class:  # 根据标签区分预测正确样本的模型平均特征归因
-                result_list.append(result)
+            # if result.pred_label == result.truth_label == mean_class:  # 根据标签区分预测正确样本的模型平均特征归因
+            #     result_list.append(result)
             # if result.pred_label == mean_class:   # 根据预测标签区分的模型平均特征归因（不分区是否预测正确）
             #     result_list.append(result)
             # if result.truth_label == mean_class:   # 根据真是标签区分的模型平均特征归因（不分区是否预测正确）
             #     result_list.append(result)
-            # if result.pred_label == result.truth_label:   # 所有预测正确样本的总体特征归因（不分区标签）
-            #     result_list.append(result)
+            if result.pred_label == result.truth_label:   # 所有预测正确样本的总体特征归因（不分区标签）
+                result_list.append(result)
             # result_list.append(result)  # 模型的总体特征归因（不分区标签）
 
-        fig, heatmap_channel, top_channels = class_mean_plot(result_list, channels_info, label=mean_class,
-                                                             top_channel_num=10)
+        fig, heatmap_channel, _ = class_mean_plot(result_list, channels_info, top_channel_num=5)
         save_figure(fig, '../plot/heatmap/', '{}_{}_mean'.format(model_name_list[model_id], mean_class))
         for i in heatmap_channel_list:
             cos_sim = cosine_similarity(i.reshape(1, -1), heatmap_channel.reshape(1, -1))
