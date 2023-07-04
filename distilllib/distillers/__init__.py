4967fe5b4bf6fd8be7da7ae4b6a47490fd4324be
hitfyd <hitfyd@foxmail.com>
2023-07-04 17:17:28 +0800

    删除CLKD

--- distilllib/distillers/__init__.py
+++ distilllib/distillers/__init__.py
@@ -3,7 +3,6 @@ from .KD import KD
 from .ESKD import ESKD
 from .GKD import GKD
 from .DKD import DKD
-from .CLKD import CLKD
 from .MSEKD import MSEKD
 from .ShapleyFAKD import ShapleyFAKD
 
@@ -14,6 +13,5 @@ distiller_dict = {
     "GKD": GKD,
     "MSEKD": MSEKD,
     "DKD": DKD,
-    "CLKD": CLKD,
     "ShapleyFAKD": ShapleyFAKD,
 }
