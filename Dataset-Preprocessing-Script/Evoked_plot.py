6b7c6c4fa7c0a60144493937556bf2055b352d1e
hitfyd <hitfyd@foxmail.com>
2023-10-08 12:56:00 +0800

    补充Evoked诱发相应地形图绘制

---
+++ Dataset-Preprocessing-Script/Evoked_plot.py
@@ -0,0 +1,85 @@
+# 将CamCAN数据集中的visual-audio passive任务数据进行预处理，每个subject的epoch单独存储为一个.npz文件
+# 预处理流程：带通滤波[1, 45]Hz，时间区间为[-0.296, 0.503]s，取梯度计数据，1/8降采样
+# Reference：
+# [1] J. R. Taylor et al., “The Cambridge Centre for Ageing and Neuroscience (Cam-CAN) data repository: Structural and functional MRI, MEG, and cognitive data from a cross-sectional adult lifespan sample,” NeuroImage, vol. 144, pp. 262–269, Jan. 2017, doi: 10.1016/j.neuroimage.2015.09.018.
+# [2] I. Zubarev, R. Zetter, H.-L. Halme, and L. Parkkonen, “Adaptive neural network classifier for decoding MEG signals,” Neuroimage, vol. 197, pp. 425–434, Aug. 2019, doi: 10.1016/j.neuroimage.2019.04.068.
+# [3] https://www.cam-can.com/
+
+import mne
+import os
+from glob import glob
+from multiprocessing import Pool
+import numpy as np
+import traceback
+
+# MEG RAW 预处理参数
+freq_min = 1.
+freq_max = 45.
+t_min = -0.296
+t_max = 0.503
+ch_type = 'grad'
+ch_reject = {'grad': 4000e-13}
+decimate = 8
+
+# MEG源文件路径、epochs存储路径
+data_path = 'D:/Cam-CAN/meg_passive_raw/'
+raw_format = '/ses-passive/meg/sub-CC*_ses-passive_task-passive_proc-sss.fif'
+# Epochs存储路径
+save_path = 'D:/Cam-CAN/freq{}-{}_{}_decimate{}/'.format(freq_min, freq_max, ch_type, decimate)
+if not os.path.exists(save_path):
+    os.mkdir(save_path)
+
+
+# npz文件存储两个键值
+# 'data'为epochs数据List
+# 'labels'为对应事件标签List
+def raw2epochs(subject, cover=False):
+    sub_id_name = subject[-12:]
+    # 当cover=False时，判断是否已经处理过，存在该subject的epochs文件则跳过
+    if not cover and len(glob(save_path + sub_id_name + '*')) > 0:
+        print(subject, 'has been preprocessed!')
+        return
+    # 获取源文件绝对路径
+    meg_raw_filename = glob(subject + raw_format)[0]
+    try:
+        # 读取源文件并加载到内存中，设置日志级别为‘CRITICAL’（最低）
+        raw = mne.io.read_raw_fif(meg_raw_filename, preload=True, verbose='CRITICAL')
+        # 滤波
+        raw = raw.filter(l_freq=freq_min, h_freq=freq_max)
+        # 读取’STI101‘刺激信道上的事件及其开始时间，事件持续时间最小为0.003s
+        events = mne.find_events(raw, stim_channel='STI101', min_duration=0.003, output='onset')
+        # 选择所有梯度计
+        picks = mne.pick_types(raw.info, meg=ch_type)
+        # 提取epochs，设置降采样，拒绝阈值
+        epochs = mne.epochs.Epochs(raw, events, tmin=t_min, tmax=t_max, decim=decimate,
+                                   detrend=1, preload=True, picks=picks, reject=ch_reject)
+        # 平衡样本标签数量
+        epochs.equalize_event_counts(['6', '7', '8'])
+        epochs.equalize_event_counts([['6', '7', '8'], '9'])
+
+        times = np.arange(0.05, 0.25, 0.01)
+        cmap = 'Oranges'
+        aud_evoked = epochs['6', '7', '8'].average()
+        vis_evoked = epochs['9'].average()
+        aud_evoked.plot_topomap(times, ch_type=ch_type, average=0.02, cmap=cmap, sensors=False, ncols=4, nrows="auto")
+        vis_evoked.plot_topomap(times, ch_type=ch_type, average=0.02, cmap=cmap, sensors=False, ncols=4, nrows="auto")
+        # aud_evoked.plot_topomap(times, ch_type=ch_type, cmap=cmap, sensors=False, ncols=4, nrows="auto")
+        # vis_evoked.plot_topomap(times, ch_type=ch_type, cmap=cmap, sensors=False, ncols=4, nrows="auto")
+
+    except IOError:
+        print(subject, IOError)
+        traceback.print_exc()
+    except ValueError:
+        print(subject, ValueError)
+        traceback.print_exc()
+    except KeyError:
+        print(subject, KeyError)
+        traceback.print_exc()
+
+
+if __name__ == '__main__':
+    sub_dirs = glob(data_path + 'sub-CC*')
+    # 按照受试者编号从小到大排序
+    sub_dirs.sort()
+    raw2epochs(sub_dirs[204], True)
+
